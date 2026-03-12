d90155cb630d11757ce462a94afd900e76e8886b
achimdehnert <achim.dehnert@iil.gmbh>
2026-03-12 17:06:01 +0100

    fix: Remove iil-django-commons, add rest_framework + whitenoise
    - iil-django-commons not on public PyPI yet
    - rest_framework needed in INSTALLED_APPS for iil-learnfw[api]
    - whitenoise middleware for static file serving

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party
+    "rest_framework",
     # IIL Platform Packages
     "iil_learnfw",
     # Hub Apps
@@ -29,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
